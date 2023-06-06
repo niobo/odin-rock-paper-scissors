8d55b1f287f9a2a3b043d1f443a58a662f8ff810
deltanoru <cleanest@internet.ru>
2023-06-06 17:38:02 +0300

    Create UI
    Add background image + set items images
    Change the delete the old game function
    Create COMMENTS

--- script.js
+++ script.js
@@ -10,45 +10,42 @@ function getComputerChoice() {
 }
 
 
+// returns 1 if the player won, 0 if compute won, -1 if draw
 function playRound (playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    let lose_msg = `Looser! ${computerSelection} beats ${playerSelection}!`,
-      win_msg = `Nice, G! ${playerSelection} beats ${computerSelection}`,
-      draw_msg = 'Draw!';
-    
 
     if (playerSelection == 'rock') {
         if (computerSelection == 'paper') {
             computerWins++;
-            return lose_msg;
+            return 0;
         }
         if (computerSelection == 'scissors') {
             playerWins++;
-            return win_msg;
+            return 1;
         }
-        return draw_msg;
+        return -1;
     
     } else if (playerSelection == 'paper') {
         if (computerSelection == 'scissors') {
             computerWins++;
-            return lose_msg;
+            return 0;
         }
         if (computerSelection == 'rock') {
             playerWins++;
-            return win_msg;
+            return 1;
         }
-        return draw_msg;
+        return -1;
 
     } else if (playerSelection == 'scissors') {
         if (computerSelection == 'rock') {
             computerWins++;
-            return lose_msg;
+            return 0;
         }
         if (computerSelection == 'paper') {
             playerWins++;
-            return win_msg;
+            return 1;
         }
-        return draw_msg;
+        return -1;
     
     } else {
         return false;
@@ -56,29 +53,50 @@ function playRound (playerSelection, computerSelection) {
 }
 
 
-function game () {
-    for (let i = 0 ; i < 5 ; i++) {
-        let playerSelection = prompt("Rock, Paper or Scissors?");
-        let computerSelection = getComputerChoice();
-
-        if (playerSelection === null) {
-            return;
-        }
-
-        let result = playRound(playerSelection, computerSelection);
-
-        // player typed something else from rock/paper/scissors
-        if (!result) {
-            i--;
-            console.log("Doesn't count! You are stuupid!")
-        } else {
-            console.log(result);
-        }
-    }
-    console.log((playerWins > computerWins) ? "Nice work G! You win!" : "You're a looser!")
+// sets everything to 0 and disables btns
+function clearGame () {
+    playerWins = 0;
+    computerWins = 0;
+    player_score_node.textContent = 0;
+    computer_score_node.textContent = 0;
+    winner_place.textContent = "";
+    player_btns.forEach((btn) => btn.removeAttribute("disabled"));
 }
 
 
-let playerWins = 0,
-    computerWins = 0;
-game();
+// starts a round and checks if the game is over
+function game (key) {
+    // data-key contains the choice (rock | paper | scissors)
+    playRound(key.getAttribute("data-key"), getComputerChoice());
+    player_score_node.textContent = `${playerWins}`;
+    computer_score_node.textContent = `${computerWins}`;
+
+    if (playerWins == 5) {
+        winner_place.textContent = "You won!";
+        player_btns.forEach((btn) => btn.setAttribute("disabled", "disabled"));
+    } else if (computerWins == 5) {
+        winner_place.textContent = "You lost!";
+        player_btns.forEach((btn) => btn.setAttribute("disabled", "disabled"));
+    }
+}
+
+
+const player_score_node = document.querySelector('.score-container .player-score');
+const computer_score_node = document.querySelector('.score-container .computer-score');
+
+const winner_place = document.querySelector(".score .winner");
+const reset_div = document.querySelector(".reset");
+
+const reset_btn = document.createElement('button');
+reset_btn.textContent = 'RESET'
+reset_btn.classList.add('reset-btn');
+reset_btn.addEventListener('click', clearGame);
+reset_div.appendChild(reset_btn);
+
+let playerWins = 0, computerWins = 0;
+
+const player_btns = document.querySelectorAll(".player-side button");
+player_btns.forEach((key) => key.addEventListener(
+    'click',
+    () => game(key)
+));
